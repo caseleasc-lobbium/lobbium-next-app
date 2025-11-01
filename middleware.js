d41144cd138750658a3ef038ee255c8139a2e2df
CaselLobbium <info@lobbium.com>
2025-11-01 02:00:20 +0100

    Fix middleware redirect for maintenance mode

--- middleware.js
+++ middleware.js
@@ -1,5 +1,12 @@
 import { NextResponse } from "next/server";
 
-export function middleware() {
-  return NextResponse.redirect(new URL("/maintenance", request.url));
+export function middleware(request) {
+  // Weiterleitung auf /maintenance, aber nur, wenn der Pfad nicht schon /maintenance ist
+  if (!request.nextUrl.pathname.startsWith("/maintenance")) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/maintenance";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 }
